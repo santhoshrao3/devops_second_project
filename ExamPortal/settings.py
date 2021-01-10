318646ae883e75547342b477123fbd73f838e584
ShivamBhirud <shivambhirudpr@gmail.com>
2021-01-10 19:23:14 +0530

    deployment changes

--- ExamPortal/settings.py
+++ ExamPortal/settings.py
@@ -8,8 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = '1x+r^6_zw$obx-7&j8t3b1&w0juiq4h0b47@2#tb9i8im^iw+f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 DEBUG = 'False'
 
 ALLOWED_HOSTS = ['localhost', 
--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn portfolio.wsgi:application --log-file - --log-level debug
+web: gunicorn ExamPortal.wsgi:application --log-file - --log-level debug
 python manage.py collectstatic --noinput
 release: python manage.py migrate --noinput
